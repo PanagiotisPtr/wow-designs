e184214de2a854fb4525adedfa084105a4994eb4
PanagiotisPtr <lpanagiotispetridisl@gmail.com>
2019-09-07 11:01:55 +0100

    fixed bug with the product page (suggestion images were too large)

--- src/Components/ClothesList/ClothesList.js
+++ src/Components/ClothesList/ClothesList.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './ClothesList.css';
 
-const Item = ({photo, title, price, colors}) => 
+const Item = ({photo, title, price, colors, sizeClass}) => 
   <div className="col-container align-center item-details">
-    <img src={photo} alt={title} className="photo clickable"/>
+    <img src={photo} alt={title} className={'photo clickable ' + sizeClass}/>
     <span className="title clickable">{title}</span>
     <span className="price">{price}</span>
     <div className="row-container justify-center colors">
@@ -15,10 +15,10 @@ const Item = ({photo, title, price, colors}) =>
 
 const ClothesList = ({clothes, preview, size}) => {
   const fullScreen = preview ? '' : 'full-width full-height';
-  const smaller = size === 'small' ? 'smaller-images' : '';
+  const smaller = size === 'small' ? 'smaller-image' : '';
   return (
-    <div className={'row-container clothes-list ' + fullScreen + ' ' + smaller}>
-      {clothes && clothes.map(item => <Item {...item} />
+    <div className={'row-container clothes-list ' + fullScreen}>
+      {clothes && clothes.map(item => { item.sizeClass = smaller; return <Item {...item} /> }
       )}
     </div>
   );
